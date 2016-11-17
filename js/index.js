5abdc81825ece7b020c9939c37affaaaaa13bcf9
berziiii <berzelba@gmail.com>
2016-11-17 12:30:10 -0500

    attempt to fix constent prompt above 800px for scroll down

--- js/index.js
+++ js/index.js
@@ -20,17 +20,17 @@ $(function(){
   });
 
   $(window).resize(function() {
-    if ($(window).width() < 800) {
+    if ($(window).width() <= 800) {
       $(".arrow").show('slow').delay( 3000).hide('slow');
     } else if ($(window).height() > 1050) {
-      $(".arrow").addClass('hide');
+      $(".arrow").hide();
     } else if ($(window).width() < 767) {
       $(".modal-box").addClass('fixed');
       $(".modal-box").removeClass('flex');
     } else {
       $(".modal-box").addClass('flex');
       $(".modal-box").removeClass('fixed');
-      // $(".arrow").addClass('hide');
+      $(".arrow").hide();
     }
   });
 
