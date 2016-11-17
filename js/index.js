ad8e138dc96e1184bb330b23e6adf74fc1616875
berziiii <berzelba@gmail.com>
2016-11-17 08:56:48 -0500

    Consistency through pop ups
    - Pop ups are all same html code base
    - adjusted window resize to allow for media break point
    - Still working on responsiveness with pop up content
    - No work done on click handlers on image

--- js/index.js
+++ js/index.js
@@ -5,15 +5,15 @@ $(function(){
     $('a[data-modal-id]').click(function(e) {
       e.preventDefault();
       $("body").append(appendthis);
-      $(".modal-overlay").fadeTo(500, 0.7);
-      //$(".js-modalbox").fadeIn(500);
+      $(".modal-overlay").fadeTo(250, .75);
+      // $(".js-modalbox").fadeIn(500);
       var modalBox = $(this).attr('data-modal-id');
       $('#'+modalBox).fadeIn($(this).data());
     });
 
 
   $(".js-modal-close, .modal-overlay").click(function() {
-      $(".modal-box, .modal-overlay").fadeOut(500, function() {
+      $(".modal-box, .modal-overlay").fadeOut(250, function() {
           $(".modal-overlay").remove();
       });
 
